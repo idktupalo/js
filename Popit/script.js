d7a3e402f3ab852598b90baa5227b9ebf811ebbc
idktupalo <70593424+idktupalo@users.noreply.github.com>
2021-05-28 14:59:25 +0300

    Delete Popit directory

--- Popit/script.js
+++
@@ -1,9 +0,0 @@
-const bubble = document.getElementsByClassName("popit_bubble");
-const music = new Audio('pop.mp3');
-
-[].forEach.call(bubble,(event)=>{
-    event.addEventListener('click',()=>{
-        music.play();
-    })
-})
-
